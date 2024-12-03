c50031a63a28400b3debc3ba015a3ac08a157219
NimeshSNM <nimesh@seattlenewmedia.com>
2024-12-03 18:48:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
